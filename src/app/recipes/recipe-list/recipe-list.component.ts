dccfe52c763d53c2025a92eaac61116618e141b7
Ed Natividad <mtdenatividad@gmail.com>
2017-09-27 19:58:11 -0400

    updated the project for all router changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit, EventEmitter, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -11,7 +12,9 @@ export class RecipeListComponent implements OnInit {
   //@Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] =  [];
   //new Recipe('A Test Recipe','this is simply a test','https://cdn.pixabay.com/photo/2017/04/08/14/37/kitchen-2213422_960_720.jpg');
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+            private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
@@ -19,5 +22,9 @@ export class RecipeListComponent implements OnInit {
   // onRecipeSelected(recipe: Recipe){  
   //   this.recipeWasSelected.emit(recipe);
   // }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
 
 }
